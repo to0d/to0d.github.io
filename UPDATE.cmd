c4ce4d015f485430b3fc41724d3602fc87447727
liuyan <liuyan3000@hotmail.com>
2017-03-22 13:52:26 +0800

    add test page

--- UPDATE.cmd
+++ UPDATE.cmd
@@ -1,4 +1,4 @@
 @echo off  
 E:
 cd E:\work\atool_uni 
-call bin\alpha-note.cmd main.DoUpdateNote public-note  "E:\work\note\04 Public" E:\work\to0d-blog\public -expand-all "--path=E:\work\to0d-blog\conf"
+call bin\alpha-note.cmd main.DoUpdatePublic "E:\work\note\04 Public" E:\work\to0d-blog\public "--path=E:\work\to0d-blog\conf"
