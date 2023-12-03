e99410b52b2e11970bf8606544630d73df818d49
todd <to0d@outlook.com>
2023-12-03 21:43:10 +0800

    weekly update

--- BLOG-LIST-UNREF.cmd
+++ BLOG-LIST-UNREF.cmd
@@ -1,5 +1,5 @@
 @echo off  
 C:
 cd C:\data\note\inote
-call bin\alpha_note_blog.cmd main.DoListUnrefNodes C:\data\note\inote\conf\.note.properties "-base=C:" "-dst=C:\data\blog\blog_public" "-main=C:\data\note\note_public" --notOutputStatus
+call bin\alpha_note_blog.cmd main.DoListUnrefNodes C:\data\note\inote\conf\.note.properties "-base=C:" "-dst=C:\data\public\to0d.github.io" "-main=C:\data\note\note_public" --notOutputStatus
 pause
